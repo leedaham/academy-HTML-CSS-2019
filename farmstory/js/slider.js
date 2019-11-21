a2dec7da4b819ec6f1d38dd3893f4e77f6476285
leedaham <mydaham1004@gmail.com>
2019-11-21 11:04:36 +0900

    2019/11/21 upload

--- farmstory/js/slider.js
+++ farmstory/js/slider.js
@@ -1,22 +1,11 @@
 $(function(){
-    var slides = $('.slider > ul > li');
-    var i = 0;    
+    $('.slider > ul').bxSlider({
+        slideWidth: 980,
+        auto:true,
+        pause:3000,
+        controls:false,
+        pager:false,
+        touchEnabled:false
 
-    setInterval(function(){
-    
-        slides.eq(i).animate({'left':'-100%'},{
-            duration:1000,
-            easing:'linear'
-        });
-        i++;
-        if(i==3){
-            i=0
-        }
-        slides.eq(i).css('left', '100%').animate({'left':'0%'},{
-            duration:1000,
-            easing:'linear'
-        });
-
-    },1000*3);
-
+    });
 });
